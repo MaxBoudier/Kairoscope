f718fcdab36dd1a23e609d5575b903fcba87b865
Cieslar-arthur <arthur.cieslar.auditeur@lecnam.net>
2026-02-01 21:01:48 +0100

    correction de mon nom shw

--- webapp/front/src/pages/Home.jsx
+++ webapp/front/src/pages/Home.jsx
@@ -253,7 +253,7 @@ const Home = () => {
               linkedin: "https://www.linkedin.com/in/boudier-maxime/"
             },
             {
-              name: "Arthur Cielsar",
+              name: "Arthur Cieslar",
               role: "Développeur full-stack",
               school: "Licence Informatique Générale au CNAM (3e année)",
               desc: "Arthur est en charge du développement front-end et back-end de l’application Kairoscope. Il est également en charge de la mise en place de la base de données, de la gestion des utilisateurs et du montage de la vidéo de présentation.",
@@ -315,6 +315,7 @@ const Home = () => {
                     <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
                   </svg>
                 </a>
+                <span> LINKEDIN </span>
               </div>
 
             </article>
